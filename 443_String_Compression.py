9c000f56ec58b569cfffae7f07c8db56b450b7d3
Mizanur Rahman <mizan.dimik@gmail.com>
2021-11-17 23:43:55 +0600

    String Compression

--- 443_String_Compression.py
+++ 443_String_Compression.py
@@ -1,17 +1,34 @@
-class Solution:
-    def compress(self, chars: List[str]) -> int:
-        if len(chars) == 1:
-            return 1
-        ans = 0
-        current_index = 0
-        items_checked = []
-        i = 0
-        while i < len(chars):
-            current_item = chars[i]
-            ans = ans + 2
-            for j in chars[i:len(chars)]:
+def compress(chars):
+    """
+    :type chars: List[str]
+    :rtype: int
+    """
+    if len(chars) == 1:
+        return 1
+    index_completed = 0
+    i = 0
+    while i < len(chars):
+        current_item = chars[i]
+        count = 0
+        for j in chars[i:len(chars)]:
+            if j != current_item:
+                break
+            else:
+                count = count +1
                 i = i + 1
-                if j != current_item:
-                    break
 
-        return ans
+        if index_completed == 0:
+            chars = [current_item, count] + chars[i:len(chars)]
+        else:
+            chars = chars[0:index_completed] + [current_item, count] + chars[i:len(chars)]
+        index_completed = index_completed + 2
+        i = index_completed
+    return len(chars)
+
+if __name__ == '__main__':
+    test = ["a","a","a","b","b","a","a"]
+    test = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+    test = ["a"]
+    test = ["a","a","b","b","c","c","c"]
+    ans = compress(test)
+    print(ans)
